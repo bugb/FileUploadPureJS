57ad2f3fd7325179a7c1b625dedf3da1056bfa51
Chau Giang <giang.ntc@temando.com>
2018-12-17 17:25:13 +0700

    Updates

---
+++ precache-manifest.dbbdd27c5ad6f31881c9e6433fe5b6f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5547461917661190eedc",
+    "url": "/FileUploadPureJS/static/js/runtime~main.55474619.js"
+  },
+  {
+    "revision": "7c465cd2a78fe512dfdd",
+    "url": "/FileUploadPureJS/static/js/main.7c465cd2.chunk.js"
+  },
+  {
+    "revision": "fb91a35d1555c6265a22",
+    "url": "/FileUploadPureJS/static/js/1.fb91a35d.chunk.js"
+  },
+  {
+    "revision": "7c465cd2a78fe512dfdd",
+    "url": "/FileUploadPureJS/static/css/main.ff15ae57.chunk.css"
+  },
+  {
+    "revision": "3bad388328212f1fef29dc50289ce560",
+    "url": "/FileUploadPureJS/index.html"
+  }
+];
